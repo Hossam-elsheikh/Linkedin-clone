09ebd7ca347ccd0d3baa3f635d8bc7af7e2b475d
EssamKonafa <essamazoz9@gmail.com>
2024-01-22 15:12:25 +0200

    Merge changes from main

--- app/network/page.jsx
+++ app/network/page.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Footer from "@/components/Footer/Footer";
+import NetNav from "@/components/Network/NetNav";
+import Network from "@/components/Network/Network";
+import React from "react";
 
 function page() {
   return (
-    <div>network</div>
-  )
+    <div className="w-full xl:w-3/4 grid grid-cols-4 m-auto gap-6 p-2 md:py-6">
+      <div className="col col-span-4 lg:col-span-1 ">
+        <NetNav/>
+        <Footer/>
+      </div>
+      <div className="col col-span-4 lg:col-span-3 ">
+        <Network/>
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default page;
