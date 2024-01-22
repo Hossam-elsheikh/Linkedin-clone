cec1df1ba3eaea0a8f46e5ffd48f80986f25e1c9
Hossam-Elsheikh <hossam.m.elsheikh@gmail.com>
2024-01-22 15:14:48 +0200

    a

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,9 +2,7 @@
 import React, { useEffect, useState } from "react";
 import Container from "../ui/Container";
 import UserDetails from "@/components/Profile/UserDetails";
-<<<<<<< HEAD
 import People from "@/components/Profile/People";
-=======
 import useFetchUser from "./useFetchUser";
 import Cookies from "js-cookie";
 
@@ -12,19 +10,12 @@ const Page = () => {
 
   const userId = Cookies.get('userId')
   const userDetails = useFetchUser(userId)
->>>>>>> 5136eef16d3d7ce570e6b6fbfd92129efcb1e8a6
 
   return (
-<<<<<<< HEAD
-    <div className="grid grid-cols-7 w-full xl:w-3/4  m-auto gap-6 p-2 md:py-6 relative">
-      <div className=" col col-span-7  lg:col-span-5">
-        <UserDetails/>
-=======
     <div className="grid grid-cols-7 w-full xl:w-3/4  m-auto gap-6 p-2 md:py-6">
       <div className=" col col-span-5">
         <UserDetails userDetails={userDetails} />
         
->>>>>>> 5136eef16d3d7ce570e6b6fbfd92129efcb1e8a6
       </div>
       <div className="col col-span-7  lg:col-span-2 relative">
         <People/>
